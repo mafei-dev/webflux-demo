dbf7c3752a00d8c4c711386f383f3e7e5c02bc06
mafei <mafei.dev@gmail.com>
2021-06-30 17:52:25 +0530

    completed with flux non-blocking way. [without webflux]

---
+++ src/main/java/com/mafei/webfluxdemo/controller/ReactiveMathController.java
@@ -0,0 +1,38 @@
+package com.mafei.webfluxdemo.controller;
+
+import com.mafei.webfluxdemo.dto.Response;
+import com.mafei.webfluxdemo.service.MathService;
+import com.mafei.webfluxdemo.service.ReactiveMathService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+/*
+  @Author mafei
+  @Created 6/30/2021 5:18 PM  
+*/
+@RestController
+@RequestMapping("/reactive/math")
+public class ReactiveMathController {
+
+    @Autowired
+    private ReactiveMathService reactiveMathService;
+
+
+    @GetMapping("/square/{input}")
+    public Mono<Response> findSquare(@PathVariable("input") int input) {
+        return this.reactiveMathService.findSquare(input);
+    }
+
+    @GetMapping("/table/{input}")
+    public Flux<Response> multiplicationTable(@PathVariable("input") int input) {
+        return this.reactiveMathService.multiplicationList(input);
+    }
+}
---
+++ src/main/java/com/mafei/webfluxdemo/service/ReactiveMathService.java
@@ -0,0 +1,45 @@
+package com.mafei.webfluxdemo.service;
+
+import com.mafei.webfluxdemo.dto.Response;
+import com.mafei.webfluxdemo.utils.SleepUtil;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/*
+  @Author mafei
+  @Created 6/30/2021 5:09 PM  
+*/
+@Service
+@Log4j2
+public class ReactiveMathService {
+
+    public Mono<Response> findSquare(int input) {
+        return Mono.fromSupplier(() -> {
+            return input * input;
+        }).map(Response::new);
+    }
+
+    public Flux<Response> multiplicationList(int input) {
+        return Flux.range(1, 10)
+                .doOnNext(
+                        i -> {
+                            SleepUtil.sleepSeconds(1);
+                        }
+                )
+                .doOnNext(
+                        i -> {
+                            log.debug("processing : {}", i);
+                        }
+                ).map(
+                        i -> {
+                            return new Response(i * input);
+                        }
+                );
+    }
+}
